2ff178b6618e6f6257a8234c7d457ba8ed59327d
Tiago Lima <tiago@myurbanjungle.com>
2020-12-07 17:00:23 +0000

    Exercise 4 fix...

--- python_app/ac.py
+++ python_app/ac.py
@@ -7,6 +7,9 @@ def eat_input(input_file):
     with open('../inputs/' + input_file) as f:
         return f.read().splitlines()
 
+def eat_raw_input(input_file):
+    with open('../inputs/' + input_file) as f:
+        return f.read()
 
 # PROBLEM 1
 
@@ -86,4 +89,48 @@ print(
     * "".join([ row_modifier(row,1,5,index) for index, row in enumerate(input_3)]).count("X")
     * "".join([ row_modifier(row,1,7,index) for index, row in enumerate(input_3)]).count("X")
     * "".join([ row_modifier(row,2,1,index) for index, row in enumerate(input_3)]).count("X"),
+)
+
+# PROBLEM 3
+
+input_4 = eat_raw_input("4.txt")
+transformer = lambda expr: re.match(
+    r"((byr:(?P<byr>.+?)|iyr:(?P<iyr>.+?)|eyr:(?P<eyr>.+?)|hgt:(?P<hgt>.+?)|hcl:(?P<hcl>.+?)|ecl:(?P<ecl>.+?)|pid:(?P<pid>.+?)|cid:(?P<cid>.+?))(\s|\n))+",
+    expr
+).groupdict()
+
+num_nones = lambda passport: functools.reduce(lambda acc, value: 1 + acc if not value else acc, passport.values(),0)
+
+def passport_rules(passport, part):
+    if num_nones(passport) == 0 or ( num_nones(passport) == 1 and not passport['cid']):
+        if part == 1:
+            return True
+        if re.match(r"(\d+)(in|cm)", passport['hgt']) is None:
+            return False
+        hgt = re.match(r"(?P<hgt>\d+)(?P<unit>(in|cm))", passport['hgt']).groupdict()
+        hcl = re.match(r"^#[a-z0-9]{6}$",passport['hcl'])
+        pid = re.match(r"(^[0-9]{9}$)", passport['pid'])
+        return all([
+            int(passport['byr']) >= 1920 and int(passport['byr']) <= 2002,
+            int(passport['iyr']) >= 2010 and int(passport['iyr']) <= 2020,
+            int(passport['eyr']) >= 2020 and int(passport['eyr']) <= 2030,
+            (int(hgt['hgt']) >= 150 and int(hgt['hgt']) <= 193 ) if hgt["unit"]=="cm" else (int(hgt['hgt']) >= 59 and int(hgt['hgt']) <= 76),
+            hcl is not None,
+            pid is not None,
+            passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        ])
+    else:
+        return False
+
+print("4.1")
+print(
+    len([
+        transformer(passport+"\n") for passport in re.split( r"\n{2}", input_4) if passport_rules(transformer(passport+"\n"), 1)
+    ])
+)
+print("4.2")
+print(
+    len([
+        transformer(passport+"\n") for passport in re.split( r"\n{2}", input_4) if passport_rules(transformer(passport+"\n"), 2)
+    ])
 )
